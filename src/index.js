5b82e4a585ca4692658b28ab740aa123be17ee7f
Nikolai200583 <125957772+Nikolai200583@users.noreply.github.com>
2023-03-14 21:14:19 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { gameDifficulty } from './gameDifficulty.js';
+
 window.application = {
     blocks: {},
     screens: {},
@@ -29,6 +31,7 @@ function renderBlockChoice(levelContent) {
         buttonsLabel.textContent = elements;
         levelContent.appendChild(buttonsLevels);
         levelContent.appendChild(buttonsLabel);
+        gameDifficulty();
     });
 }
 
@@ -42,9 +45,37 @@ function renderScreenChoice() {
     const buttonStart = document.createElement('button');
     buttonStart.classList.add('button__start');
     buttonStart.textContent = 'Старт';
-
-    window.application.renderBlock('blockChoice', levelContent);
     app.appendChild(title);
     app.appendChild(levelContent);
     app.appendChild(buttonStart);
+    window.application.renderBlock('blockChoice', levelContent);
+}
+
+export function renderScreenEndGame(titleGame) {
+    document.querySelector('body').textContent = '';
+
+    const app = document.createElement('div');
+    app.classList.add('app');
+    const title = document.createElement('h1');
+    title.classList.add('title');
+    title.textContent = titleGame;
+    const timer = document.createElement('div');
+    timer.classList.add('timer');
+    timer.textContent = 'ssdsdsdsdsdsd';
+    const buttonStart = document.createElement('button');
+    buttonStart.addEventListener('click', reloadGame);
+    buttonStart.classList.add('button__start');
+    buttonStart.textContent = 'Играть снова';
+    document.querySelector('body').appendChild(app);
+    app.appendChild(title);
+    app.appendChild(timer);
+    app.appendChild(buttonStart);
+}
+
+function reloadGame() {
+    document.querySelector('.app').textContent = '';
+    renderScreenChoice();
+}
+export function endTimer() {
+    clearInterval(timerInterval);
 }
